fa12a847afd9ed941174153bc70ff6c2a3cd1bb5
mertkanalacahan <mertkanalacahan@gmail.com>
2020-02-22 20:54:43 +0300

    Add stack with SLL

---
+++ DataStructures/StackWithSLL.js
@@ -0,0 +1,55 @@
+class Node
+{
+  constructor(val)
+  {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack
+{
+  constructor()
+  {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val)
+  {
+    var newNode = new Node(val);
+
+    if(!this.first)
+    {
+      this.first = newNode;
+      this.last = newNode;
+    }
+
+    else
+    {
+      var temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+
+    return ++this.size;
+  }
+
+  pop()
+  {
+    if(!this.first) return null;
+
+    var poppedNode = this.first;
+
+    if(this.first === this.last)
+    {
+      this.last = null;
+    }
+    
+    this.first = this.first.next;
+
+    this.size--;
+    return poppedNode.val;
+  }
+}
